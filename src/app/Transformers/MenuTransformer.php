f21b939c7dfe0be7edfc072d66284d4c3027beb0
Vũ Đức Hải <haivd@vicoders.com>
2021-01-20 09:30:25 +0700

    fix lits menu

--- src/app/Transformers/MenuTransformer.php
+++ src/app/Transformers/MenuTransformer.php
@@ -19,6 +19,8 @@ class MenuTransformer extends TransformerAbstract
             'name'       => $model->name,
             'page'       => $model->page,
             'position'   => $model->position,
+            'page_vn'     => $this->translatePage($model->page),
+            'position_vn' => $this->translatePosition($model->page, $model->position),
             'timestamps' => [
                 'created_at' => $model->created_at,
                 'updated_at' => $model->updated_at,
@@ -29,5 +31,27 @@ class MenuTransformer extends TransformerAbstract
     public function includeMenus($model)
     {
         return $this->collection($model->menus, new ItemMenuTransformer);
+    }
+
+    public function translatePage($page)
+    {
+        foreach (config('menu.page') as $key => $value) {
+            if ($page === $key) {
+                return $value['label'];
+            }
+        }
+    }
+
+    public function translatePosition($page, $position)
+    {
+        foreach (config('menu.page') as $key => $value) {
+            if ($page === $key) {
+                foreach ($value['position'] as $key => $value) {
+                    if ($position === $key) {
+                        return $value;
+                    }
+                }
+            }
+        }
     }
 }
